db015326fb1dd8226b832c94d81c120fc18784d2
cpp3Russia <75923874+cpp3Russia@users.noreply.github.com>
2026-09-21 07:21:45 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from 'vite-plugin-svgr'
 import path from 'path'
 
 export default defineConfig({
+  base: '/checkList/',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
